e36d90b4251f78276138bd07772f8b60bc6e74b3
Sobhard <155180513+Sobhard@users.noreply.github.com>
2026-02-04 14:09:13 -0500

    replaced ff with the revlib ff

--- src/main/java/frc/robot/subsystems/IntakeSubsystem.java
+++ src/main/java/frc/robot/subsystems/IntakeSubsystem.java
@@ -4,14 +4,21 @@
 
 package frc.robot.subsystems;
 
+import static edu.wpi.first.units.Units.RPM;
+import static edu.wpi.first.units.Units.RotationsPerSecond;
+
 import com.revrobotics.PersistMode;
 import com.revrobotics.ResetMode;
 import com.revrobotics.spark.SparkLowLevel.MotorType;
+import com.revrobotics.spark.SparkBase.ControlType;
+import com.revrobotics.spark.SparkClosedLoopController;
 import com.revrobotics.spark.SparkMax;
 import com.revrobotics.spark.config.SparkBaseConfig.IdleMode;
 import com.revrobotics.spark.config.SparkMaxConfig;
 
+import edu.wpi.first.math.controller.PIDController;
 import edu.wpi.first.math.controller.SimpleMotorFeedforward;
+import edu.wpi.first.units.measure.AngularVelocity;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants.IntakeConstants;
 
@@ -19,13 +26,11 @@ public class IntakeSubsystem extends SubsystemBase {
   /** Creates a new IntakeSubsystem. */
 
   private final SparkMax m_intakeMotor;
-  
-  private final SimpleMotorFeedforward m_intakeFeedforward;
+  private final SparkClosedLoopController m_intakeController;
 
   public IntakeSubsystem() {
     m_intakeMotor = new SparkMax(IntakeConstants.kIntakeMotor_CANID, MotorType.kBrushless);
-
-    m_intakeFeedforward =  new SimpleMotorFeedforward(IntakeConstants.kIntake_ks, IntakeConstants.kIntake_kv);
+    m_intakeController = m_intakeMotor.getClosedLoopController();
 
     configureMotors();
   }
@@ -34,6 +39,10 @@ public class IntakeSubsystem extends SubsystemBase {
     SparkMaxConfig m_intakeConfig = new SparkMaxConfig();
 
     m_intakeConfig.smartCurrentLimit(35).idleMode(IdleMode.kCoast);
+    m_intakeConfig.closedLoop
+      .feedForward
+        .kS(IntakeConstants.kIntake_ks)
+        .kV(IntakeConstants.kIntake_kv);
 
     m_intakeMotor.configure(m_intakeConfig, ResetMode.kResetSafeParameters, PersistMode.kPersistParameters);
   }
@@ -42,8 +51,8 @@ public class IntakeSubsystem extends SubsystemBase {
     m_intakeMotor.setVoltage(voltage);
   }
 
-  public void setIntake(double voltage) {
-    m_intakeMotor.setVoltage(m_intakeFeedforward.calculate(voltage));
+  public void setIntake(AngularVelocity speed) {
+    m_intakeController.setSetpoint(speed.in(RotationsPerSecond), ControlType.kVelocity);
   }
 
   @Override
