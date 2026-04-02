830dbb4cb2f2a5fc4c7483477788eba9ef7efd6a
Astraea Robotics <ebfirstinc@gmail.com>
2026-04-02 16:04:42 -0400

    added software limits on climb

--- src/main/java/frc/robot/subsystems/ClimbSubsystem.java
+++ src/main/java/frc/robot/subsystems/ClimbSubsystem.java
@@ -5,6 +5,7 @@ import com.ctre.phoenix6.controls.VoltageOut;
 import com.ctre.phoenix6.hardware.TalonFX;
 import com.ctre.phoenix6.signals.NeutralModeValue;
 
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class ClimbSubsystem extends SubsystemBase {
@@ -21,12 +22,21 @@ public class ClimbSubsystem extends SubsystemBase {
     }
 
     private void configureMotors() {
+        m_climbMotor.setPosition(0);
+
         TalonFXConfiguration config = new TalonFXConfiguration();
         config.CurrentLimits.SupplyCurrentLimit       = 40;
         config.CurrentLimits.SupplyCurrentLimitEnable = true;
         config.CurrentLimits.StatorCurrentLimit       = 80;
         config.CurrentLimits.StatorCurrentLimitEnable = true;
         config.MotorOutput.NeutralMode = NeutralModeValue.Brake;
+
+        // config.SoftwareLimitSwitch.ReverseSoftLimitEnable = true;
+        // config.SoftwareLimitSwitch.ReverseSoftLimitThreshold = 0;
+
+        config.SoftwareLimitSwitch.ForwardSoftLimitEnable = true;
+        config.SoftwareLimitSwitch.ForwardSoftLimitThreshold = 125.011;
+
         m_climbMotor.getConfigurator().apply(config);
     }
 
@@ -36,5 +46,9 @@ public class ClimbSubsystem extends SubsystemBase {
 
     public void stop() {
         m_climbMotor.stopMotor();
+    }
+
+    public void periodic(){
+        SmartDashboard.putNumber("Climb Pos", m_climbMotor.getPosition().getValueAsDouble());
     }
 }
